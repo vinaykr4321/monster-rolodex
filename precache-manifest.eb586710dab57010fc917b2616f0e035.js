6d7726a352397aef71b4f48c862376c2bc532d22
vinay <vinaykr4321@gmail.com>
2020-01-18 21:33:52 +0530

    Updates

---
+++ precache-manifest.eb586710dab57010fc917b2616f0e035.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97cb22eb69f03e0502b1630b26f93d6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e28ed09c7c8392d66a99",
+    "url": "/monster-rolodex/static/css/main.d7ce95b6.chunk.css"
+  },
+  {
+    "revision": "815b60ad9b576a6df034",
+    "url": "/monster-rolodex/static/js/2.df2da0bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.df2da0bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e28ed09c7c8392d66a99",
+    "url": "/monster-rolodex/static/js/main.b5ac24c1.chunk.js"
+  },
+  {
+    "revision": "6db175829222b490426d",
+    "url": "/monster-rolodex/static/js/runtime-main.172adef3.js"
+  }
+]);
